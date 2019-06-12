f3e80b1fb64021b211b335dc141d74425177d076
Dziubin Anton <a.dziubin@vr-dev.team>
2019-06-12 15:28:14 +0300

    fix pwd

--- v01/dockerfiles/djangorest/scripts/apidoc.sh
+++ v01/dockerfiles/djangorest/scripts/apidoc.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 rm -rf ./backend/djangorest/apidoc/*
-docker run -v $(PWD)/backend/djangorest:/app --name npm -i --rm -d node:10-slim bash -c "npm i -g apidoc && cd /app && apidoc -i ./quickstart -o ./apidoc"
+docker run -v $(pwd)/backend/djangorest:/app --name npm -i --rm -d node:10-slim bash -c "npm i -g apidoc && cd /app && apidoc -i ./quickstart -o ./apidoc"
 #docker exec -it nginx bash -c "chown -R www-data. /var/www/html"
