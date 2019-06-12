18f7e6e2828a9ebb3fba621ce4ef4c2f7be0b652
Dziubin Anton <a.dziubin@vr-dev.team>
2019-06-12 09:09:14 +0300

    apidock script & urls

--- v01/dockerfiles/django-restful/scripts/apidoc.sh
+++ v01/dockerfiles/django-restful/scripts/apidoc.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 rm -rf ./backend/django_restful/apidoc/*
-docker run -v $(PWD)/backend/django_restful:/app --name npm -i --rm -d node:10-slim bash -c "npm i -g apidoc && cd /app/django_restful && apidoc -i ./apps -o ./apidoc"
+docker run -v $(PWD)/backend/django_restful:/app --name npm -i --rm -d node:10-slim bash -c "npm i -g apidoc && cd /app && apidoc -i ./apps -o ./apidoc"
 #docker exec -it nginx bash -c "chown -R www-data. /var/www/html"
