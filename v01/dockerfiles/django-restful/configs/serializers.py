03932cd6e1aeb567a28783114366c013d6a76364
Dziubin Anton <a.dziubin@vr-dev.team>
2019-06-12 10:00:20 +0300

    module fix

--- v01/dockerfiles/django-restful/configs/serializers.py
+++ v01/dockerfiles/django-restful/configs/serializers.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User, Group
-from rest_framework import serializers
+from rest-framework import serializers
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
